aa914cfa605f89a52ef9a7ef27daf187820c37e9
Shalva <shalvaa26@gmail.com>
2019-12-06 05:48:05 +0000

    Done.

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,4 +8,9 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
+  # this method take the arg "show" and creates a place for it in the hash 
+  # it the passes 'name' which is an array of strings of character names
+  
+  BASE_HOA[show] << name
+  BASE_HOA[show] # return the data we have on the show we just added to the hash
 end
